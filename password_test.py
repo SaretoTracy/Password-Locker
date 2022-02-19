d2d344b24df3e00c6591a82d1259de81fed221d6
saretotracy <tracysareto@gmail.com>
2022-02-20 02:43:21 +0300

    test case to test if user objects is saved in UserList

--- password_test.py
+++ password_test.py
@@ -1,3 +1,4 @@
+from typing_extensions import Self
 import unittest # Importing the unittest module
 from user import User
 class TestUser(unittest.TestCase): #subclass class that inherits from unittest.TestCase
@@ -24,6 +25,11 @@ class TestUser(unittest.TestCase): #subclass class that inherits from unittest.T
         self.assertEqual(self.new_user.last_name,"Sareto")
         self.assertEqual(self.new_user.username,"Tate")
         self.assertEqual(self.new_user.password,"Tracy@2022")
+
+    def save_user(self):
+        ''' test case to test if user objects is saved in UserList'''  
+        self.new_user.save_user() 
+        self.assertEqual(len(User.UserList),1) 
 if __name__ == '__main__':
     unittest.main()
 
