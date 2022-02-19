db21ac51fca159a070faa8a6bedd8e840921fa82
saretotracy <tracysareto@gmail.com>
2022-02-20 01:00:40 +0300

    create new instances of a class and create the constructor with 5 arguments

--- user.py
+++ user.py
@@ -1,4 +1,15 @@
+from collections import UserList
+
+
 class User:
     """
     Class that generates new instances of user
     """
+
+    UserList = [] #empty user list
+
+    def __init__(self,first_name,last_name,username,password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = username
+        self.email = password
