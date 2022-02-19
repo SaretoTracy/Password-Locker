d3fb35c543ac0b84e31ec6ceaa9fd75988cab671
saretotracy <tracysareto@gmail.com>
2022-02-20 02:09:21 +0300

    test to see if our objects are being instantiated correctly.

--- password_test.py
+++ password_test.py
@@ -1,2 +1,30 @@
 import unittest # Importing the unittest module
 from user import User
+class TestContact(unittest.TestCase): #subclass class that inherits from unittest.TestCase
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self): #first test, test to see if our objects are being instantiated correctly.
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Tracy","Sareto","Tate","Tracy@2022") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"Tracy")
+        self.assertEqual(self.new_user.last_name,"Sareto")
+        self.assertEqual(self.new_user.username,"Tate")
+        self.assertEqual(self.new_user.password,"Tracy@2022")
+if __name__ == '__main__':
+    unittest.main()
+
+
