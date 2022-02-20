6e114b1c1d4323f6b3ed7234f9810f54f4d01d21
saretotracy <tracysareto@gmail.com>
2022-02-20 13:54:34 +0300

    created a main function that calls all the other function

--- run.py
+++ run.py
@@ -36,4 +36,76 @@ def display_user():
     '''
     Function that returns all the saved users
     '''
-    return User.display_users()
+    return User.display_user()
+
+def main():
+    print("Hello Welcome to your password-locker. What is your name?")
+    username = input()
+
+    print(f"Hello {username}. what would you like to do?")
+    print('\n')
+
+    while True:
+        print("Use these short codes : cc - create a new user, dc - display users, fc -find a user, ex -exit the user list ")
+
+        short_code = input().lower()
+
+        if short_code == 'cc':
+                print("New user")
+                print("-"*10)
+
+                print ("First name ....")
+                first_name = input()
+
+                print("Last name ...")
+                last_name = input()
+
+                print("Username ...")
+                username = input()
+
+                print("Password ...")
+                password = input()
+
+
+                save_user(create_user(first_name,last_name,username,password)) # create and save new user.
+                print ('\n')
+                print(f"New user {first_name} {last_name} created")
+                print ('\n')
+
+        elif short_code == 'dc':
+
+                if display_user():
+                        print("Here is a list of all your users")
+                        print('\n')
+
+                        for user in display_user():
+                                print(f"{user.first_name} {user.last_name} .....{user.username}")
+
+                        print('\n')
+                else:
+                        print('\n')
+                        print("You dont seem to have any users saved yet")
+                        print('\n')
+
+        elif short_code == 'fc':
+
+                print("Enter the username you want to search for")
+
+                search_username = input()
+                if check_existing_user(search_username):
+                        search_user = find_user(search_username)
+                        print(f"{search_user.first_name} {search_user.last_name}")
+                        print('-' * 20)
+
+                        print(f"username.......{search_user.username}")
+                        print(f"password.......{search_user.email}")
+                else:
+                        print("That user does not exist")
+
+        elif short_code == "ex":
+                print("Bye .......")
+                break
+        else:
+                print("I really didn't get that. Please use the short codes")
+if __name__== "__main__":
+    main()  
