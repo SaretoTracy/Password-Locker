1d6f956a4854890ada81bea3bb84b552e5795932
saretotracy <tracysareto@gmail.com>
2022-02-20 13:23:20 +0300

    implement createb user function

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 from user import User
+# functions that implement the behaviours we have created
+def create_user(first_name,last_name,username,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(first_name,last_name,username,password)
+    return new_user
