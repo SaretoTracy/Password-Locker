24adc3c918308891ad0882f5214c0a6a0bb0a003
saretotracy <tracysareto@gmail.com>
2022-02-20 16:59:38 +0300

    initialized credentiallist objects

--- credentials.py
+++ credentials.py
@@ -2,3 +2,9 @@ class Credentials:
     """
     Class that generates new instances of credentials
     """
+CredentialList = [] #empty user list
+    
+def __init__ (self,account_name,account_username,account_password):
+        self.account_name = account_name
+        self.account_username = account_username
+        self.account_password = account_password
